94403d7a41fce805c9d537662d9b1afec3892902
Himanshu <himanshu.au@gmail.com>
2020-07-27 23:19:21 -0400

    initial

---
+++ Azure/Azure AD Connect/Manual_Sync_Azure.ps1
@@ -0,0 +1,39 @@
+# Run following command on where "Azure AD Connect" Installed.
+# install Module AzureAD
+Install-Module -Name AzureAD
+
+# import Module
+Import-Module -Name AzureAD
+
+# Connect to AzureAD
+Connect-AzureAD
+
+# Stop AD Sync Scheduler 
+Set-ADSyncScheduler -SyncCycleEnabled $false
+
+<# 
+Define ConnectorName and RunProfileName Variables for ADSync Run manually
+"Get-adsyncconnector | ft Name, ConnectorTypeName, Subtype -wrap" run this command to get connectorname
+Put Active Directory Name first "xyz.com" and "abc.onmicrosoft.com - AAD" second in Varialbe Name $connectorName
+#>
+
+$ConnectorName = @("xyz.com","abc.onmicrosoft.com - AAD")
+$RunProfileName = @("Delta Import","Delta Synchronization","Export")
+
+# Run ADSyncRunProfile for each Connector Name for each Profile Name
+foreach ($RunProfile in $RunProfileName)
+{
+    foreach ($Connector in $ConnectorName)
+    {
+        write-host " "
+        write-host "$Connector connector sync with runprofile $RunProfile" -ForegroundColor Cyan
+        write-host " "
+        write-host "-----------------------------------------------------------" -ForegroundColor Yellow
+        write-host " "
+        Invoke-ADSyncRunProfile -ConnectorName $Connector -RunProfileName $RunProfile
+        Start-Sleep -Seconds 1
+    }
+}
+
+# Start AD Sync Scheduler
+Set-ADSyncScheduler -SyncCycleEnabled $true
