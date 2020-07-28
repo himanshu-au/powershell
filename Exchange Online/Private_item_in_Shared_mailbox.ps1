00e81b3cb160bff8bfedb0c418ced8c120d81aa6
Himanshu <himanshu.au@gmail.com>
2020-07-28 00:45:03 -0400

    initial

--- Exchange Online/Private_item_in_Shared_mailbox.ps1
+++ Exchange Online/Private_item_in_Shared_mailbox.ps1
@@ -1,22 +1,23 @@
-# MSOnline module must be installed.
-install-module msonline
+# Stored Office 365 credential
+$UserCredential = Get-Credential
 
-# Connect to MSOnline services
-Connect-MsolService
-
-# Connect to Exchange Online
+# connect to Powershell Session  
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
 
-# Import Powershell Session
-Import-PSSession $Session
+# import Powershell Session
+Import-PSSession $Session -DisableNameChecking
 
 # store sharedmailbox and user's email to variable
 $email = Read-host "Please Enter User e-mail ID: "
 Write-Output ""
 $sharedmailbox = Read-Host "Please Enter Sharedmailbox e-mail ID: "
+$identity = $sharedmailbox + ":\calendar"
 
 # Give Delegate and CanViewPrivateItems to John@contoso.com for Sharedmailbox@contoso.com.
-Add-MailboxFolderPermission -Identity $sharedmailbox:\Calendar -User $email -AccessRights Editor -SharingPermissionFlags Delegate,CanViewPrivateItems
+Add-MailboxFolderPermission -Identity $identity -User $email -AccessRights Editor -SharingPermissionFlags Delegate,CanViewPrivateItems
 
 # Print Command completed successfully
 Write-Host "Now User has delegate and CanViewPrivateItems Permission for Sharedmailbox."  -BackgroundColor Cyan -ForegroundColor Black
+
+# Get-MailboxFolderPermission
+Get-MailboxFolderPermission -Identity $identity 
