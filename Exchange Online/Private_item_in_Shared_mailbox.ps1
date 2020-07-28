f65e4b299f8304c093b8381e3b1cdc05a1a56c6c
Himanshu <himanshu.au@gmail.com>
2020-07-28 00:28:22 -0400

    initial

--- Exchange Online/Private_item_in_Shared_mailbox.ps1
+++ Exchange Online/Private_item_in_Shared_mailbox.ps1
@@ -6,7 +6,17 @@ Connect-MsolService
 
 # Connect to Exchange Online
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+
+# Import Powershell Session
 Import-PSSession $Session
 
+# store sharedmailbox and user's email to variable
+$email = Read-host "Please Enter User e-mail ID: "
+Write-Output ""
+$sharedmailbox = Read-Host "Please Enter Sharedmailbox e-mail ID: "
+
 # Give Delegate and CanViewPrivateItems to John@contoso.com for Sharedmailbox@contoso.com.
-Add-MailboxFolderPermission -Identity sharedmailbox@Contoso.com:\Calendar -User John@contoso.com -AccessRights Editor -SharingPermissionFlags Delegate,CanViewPrivateItems
+Add-MailboxFolderPermission -Identity $sharedmailbox:\Calendar -User $email -AccessRights Editor -SharingPermissionFlags Delegate,CanViewPrivateItems
+
+# Print Command completed successfully
+Write-Host "Now User has delegate and CanViewPrivateItems Permission for Sharedmailbox."  -BackgroundColor Cyan -ForegroundColor Black
