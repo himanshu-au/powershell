39610314ce5d7b28d95401a8de4610c7392a3960
Himanshu <himanshu.au@gmail.com>
2020-07-27 23:30:52 -0400

    intial

--- Azure/VMfindbytags.ps1
+++ Azure/VMfindbytags.ps1
@@ -9,18 +9,7 @@ $vmlist | Start-AzVM
 $vmlist | Stop-AzVM
 
 
-$action = Read-Host "Please Enter Action start or stop:" 
 
-if ($action.trim().ToLower() -eq "start")
- {  $action = $action.Trim().ToLower()
-    write-host "you have type $action" -ForegroundColor Magenta}
-elseif ($action.trim().ToLower() -eq "stop")
-{
-    $action = $action.Trim().ToLower()
-    write-host "you have type $action" -ForegroundColor green}
-else
-{
-    write-host "you have not type $action" -ForegroundColor Cyan}
 
 
 
---
+++ Basic/if else/ifelseifelse.ps1
@@ -0,0 +1,12 @@
+$action = Read-Host "Please Enter Action start or stop:" 
+
+if ($action.trim().ToLower() -eq "start")
+ {  $action = $action.Trim().ToLower()
+    write-host "you have type $action" -ForegroundColor Magenta}
+elseif ($action.trim().ToLower() -eq "stop")
+{
+    $action = $action.Trim().ToLower()
+    write-host "you have type $action" -ForegroundColor green}
+else
+{
+    write-host "you have not type $action" -ForegroundColor Cyan}
