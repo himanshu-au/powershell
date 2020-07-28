dd45fa540eb9da31515b359f67c98d011edceb5b
Himanshu <himanshu.au@gmail.com>
2020-07-28 09:59:03 -0400

    initial

--- Exchange Online/apply_license_to_multiple_users.ps1
+++ Exchange Online/apply_license_to_multiple_users.ps1
@@ -7,24 +7,17 @@ Import-Module -name MSOnline
 # Connect to MSOlservice
 Connect-MSOlservice
 
-# To get all license into you have on office 365 tenancy run following command
+# To get all licenses into office 365 tenancy, run following command
 Get-MsolAccountSku
 
-# Now we will get particular license for example, 
+# Now we will get particular license, for example, 
 Get-MsolAccountSku | Where-Object {$_.SkuPartNumber -eq "O365_BUSINESS_ESSENTIALS" } 
 
-# Now we will find which command for to apply license
+# Now we will find which command to apply licese to user
 Get-command *license*
 
 # we find Set-MsOlUserLicense to set license to user
 Set-MsolUserLicense -UserPrincipalName abc@xyz.com -AddLicenses <put license Name>
 
 # get unlicensed users and set licese to each unlicensed user
-Get-MsolUser -UnlicensedUsersOnly | Set-MsolUserLicense -AddLicenses <put license Name>
-
-
-
-
-
-
-  
+Get-MsolUser -UnlicensedUsersOnly | Set-MsolUserLicense -AddLicenses <put license Name>  
